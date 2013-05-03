1f5d3c2721804a323a3053484682eacd10182507
Keita Yamaguchi <keita.yamaguchi@gmail.com>
2013-05-03 10:46:36 +0900

    Added .simplecov file.

--- test/spec_temppath.rb
+++ test/spec_temppath.rb
@@ -1,12 +1,4 @@
 require 'simplecov'
-require 'coveralls'
-Coveralls.wear!
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-SimpleCov.start {add_filter 'test'}
-
 require 'temppath'
 
 describe 'Temppath' do
